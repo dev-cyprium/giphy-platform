92d1c499049b87ee7b312e28d7ae223b29580d4b
Stefan Kupresak <stefan_vg@hotmail.com>
2018-08-27 22:48:23 +0200

    Change bootstrap theme color, progress on the modal

--- resources/assets/js/components/NewReportModal.vue
+++ resources/assets/js/components/NewReportModal.vue
@@ -1,5 +1,5 @@
 <template>
-    <modal :show="'new-report-modal' == openModal" @close="close" >
+    <modal :show="'new-report-modal' === openModal" @close="close" >
         <div class="modal-header">
             <h3>Report</h3>
         </div>
@@ -8,11 +8,14 @@
                 <label>
                     Description
                 </label>
-                <textarea v-model="description" rows="5" class="form-control"></textarea>
+                <textarea @input="onInput()" v-model="description" rows="5" class="form-control"></textarea>
             </div>
         </div>
-        <div class="modal-footer text-right">
-            <button class="btn btn-primary" @click="report()">
+        <div class="modal-footer">
+            <div class="w-100">
+                <p>{{ charactersLeft }}</p>
+            </div>
+            <button class="btn btn-primary" :disabled="charactersUsed > maxCharacters" @click="report()">
                 Submit
             </button>
         </div>
@@ -24,6 +27,10 @@
     margin-top: 0;
     color: #42b983;
 }
+
+.modal-footer p {
+    font-size: 1.6rem;
+}
 </style>
 
 <script>
@@ -31,16 +38,26 @@
         props: ['openModal'],
         data: function() {
             return {
-                description: ''
+                description: '',
+                charactersUsed: 0
             }
         },
+        computed: {
+            charactersLeft() {
+                return Math.max(0, this.maxCharacters - this.charactersUsed);
+            },
+            maxCharacters: () => 50
+        },
         methods: {
-            report: function() {
+            report() {
                 this.close();
             },
-            close: function() {
+            close() {
                 this.$emit('close');
                 this.description = '';
+            },
+            onInput() {
+                this.charactersUsed = this.description.length;
             }
         }
     }
