6e1a4709bfe5c60fb21d1d88e84be7109ac511df
Stefan Kupresak <stefan_vg@hotmail.com>
2019-01-19 23:03:00 +0100

    Finish comments component

--- resources/assets/js/components/Comment.vue
+++ resources/assets/js/components/Comment.vue
@@ -1,5 +1,39 @@
 <template>
   <form>
-    <input class="post-comment-form__input" type="text" :placeholder="$t('comments.new')">
+    <div class="post-comment-form__count-wrap">
+      <input 
+      @focus="visible = true" 
+      @blur="visible = false"
+      @input="handleKeyUp"
+      :class="[
+      {'post-comment-form__input--danger': (char_left < 0)},
+      'post-comment-form__input'
+      ]" 
+      type="text" 
+      :placeholder="$t('comments.new')">
+      <span v-show="visible" :class="[
+        {'post-comment-form__number--danger': (char_left < 10)},
+        'post-comment-form__number'
+      ]">
+        {{ char_left }}
+      </span>
+    </div>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      char_left: 50,
+      visible: false
+    }
+  },
+  methods: {
+    handleKeyUp(ev) {
+      let charsInInput = ev.target.value.length;
+      this.char_left = 50 - charsInInput;
+    }
+  }
+}
+</script>
