3c4228b6855b7a65478c45839ea4e31859474d93
Stefan Kupresak <stefan_vg@hotmail.com>
2019-01-21 11:18:51 +0100

    add comment limits, add comment submit method

--- resources/assets/js/components/Comment.vue
+++ resources/assets/js/components/Comment.vue
@@ -1,16 +1,17 @@
 <template>
-  <form>
+  <form @submit.prevent="handleFormSubmit">
     <div class="post-comment-form__count-wrap">
       <input 
-      @focus="visible = true" 
-      @blur="visible = false"
-      @input="handleKeyUp"
-      :class="[
-      {'post-comment-form__input--danger': (char_left < 0)},
-      'post-comment-form__input'
-      ]" 
-      type="text" 
-      :placeholder="$t('comments.new')">
+        @focus="visible = true" 
+        @blur="visible = false"
+        @input="handleKeyUp"
+        :class="[
+        {'post-comment-form__input--danger': (char_left < 0)},
+        'post-comment-form__input'
+        ]" 
+        type="text" 
+        v-model="comment"
+        :placeholder="$t('comments.new')">
       <span v-show="visible" :class="[
         {'post-comment-form__number--danger': (char_left < 10)},
         'post-comment-form__number'
@@ -26,13 +27,21 @@ export default {
   data() {
     return {
       char_left: 50,
-      visible: false
+      visible: false,
+      comment: ''
     }
   },
   methods: {
     handleKeyUp(ev) {
       let charsInInput = ev.target.value.length;
       this.char_left = 50 - charsInInput;
+      if(this.char_left < -99) {
+        this.char_left = -99;
+      }
+    },
+    handleFormSubmit() {
+      this.comment = '';
+      this.char_left = 50;
     }
   }
 }
